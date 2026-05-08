ef48812abd6b8e2d3bf1a8396b367f4d1e249f6c
M.Abdullah_Anwar <mabdullahanwar85@gmail.com>
2026-05-08 21:24:04 +0500

    Done

--- src/components/State.jsx
+++ src/components/State.jsx
@@ -1,47 +1,49 @@
 import { useState } from "react";
 
-const State = () => {
-  let [count, setCount] = useState(0);
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [numberColor, setNumberColor] = useState("white"); 
 
-  console.log("Render");
+  const increment = () => {
+    setCount(count + 1);
+    setNumberColor("#00ff88"); 
+  };
+
+  const decrement = () => {
+    if (count > 0) {
+      setCount(count - 1);
+      setNumberColor("#ff0088"); 
+    }
+  };
 
   return (
     <div className="main">
-      
       <div className="glow1"></div>
       <div className="glow2"></div>
 
       <div className="card">
+        <h1 className="title">Hello, I am Abdullah</h1>
+        <h2 className="heading">Counter App</h2>
 
-        <h1 className="title">
-          Hello, I am Abdullah
-        </h1>
-
-        <h2 className="heading">
-          Counter App
-        </h2>
-
-        <p className="count">{count}</p>
-
-        <div className="btns">
-          <button
-            className="increase"
-            onClick={() => setCount(count + 1)}
-          >
-            Increase
-          </button>
-
-          <button
-            className="decrease"
-            onClick={() => count > 0 && setCount(count - 1)}
-          >
-            Decrease
-          </button>
+        <div className={`count ${numberColor}`}>
+          <span>{count}</span>
         </div>
 
+        <div className="btns">
+          <button className="increase" onClick={increment}>
+            + Increase
+          </button>
+          <button 
+            className="decrease"
+            disabled={count === 0}
+            onClick={decrement}
+          >
+            - Decrease
+          </button>
+        </div>
       </div>
     </div>
   );
 };
 
-export default State;
+export default App;
